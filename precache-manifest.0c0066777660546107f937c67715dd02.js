7878237f2f4072f45ee02efeb8c6c3771d79a0e4
Mayram Bitarov <bitarov_91@mail.ru>
2019-10-27 17:03:12 +0300

    Updates

---
+++ precache-manifest.0c0066777660546107f937c67715dd02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b2a4e1cea75ab4c76225b172e3b22e",
+    "url": "/vao-demo/index.html"
+  },
+  {
+    "revision": "21e51f2bf9596dd3b1bd",
+    "url": "/vao-demo/static/css/main.2cde180a.chunk.css"
+  },
+  {
+    "revision": "e3371b5d4b9b2cbb47aa",
+    "url": "/vao-demo/static/js/2.74ddfd2e.chunk.js"
+  },
+  {
+    "revision": "21e51f2bf9596dd3b1bd",
+    "url": "/vao-demo/static/js/main.38eff1b4.chunk.js"
+  },
+  {
+    "revision": "cd63e643fc7cb1cf5658",
+    "url": "/vao-demo/static/js/runtime~main.c341c129.js"
+  }
+]);
