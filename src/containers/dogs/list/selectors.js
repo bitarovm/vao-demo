3b10aebceaf2fb09910b5fc97e62478ec1f348d8
Mayram Bitarov <bitarov_91@mail.ru>
2019-10-29 12:18:58 +0300

    added reselec

--- src/containers/dogs/list/selectors.js
+++ src/containers/dogs/list/selectors.js
@@ -1,29 +1,12 @@
+import { createSelector } from 'reselect';
+
 import {
   filterDogsList,
   sortDogsList,
   groupDogsList,
 } from './utils';
 
-const dogsGroupsSelector = (state) => {
-  const {
-    dogsList,
-    searchString,
-    sortParams,
-    groupParams,
-  } = state;
-
-  let result = dogsList;
-
-  if (searchString) {
-    result = filterDogsList(result, searchString);
-  };
-
-  result = sortDogsList(result, sortParams);
-
-  result = groupDogsList(result, groupParams);
-
-  return result;
-};
+const dogsListSelector = state => state.dogsList;
 
 const isLoadingSelector = state => state.isLoading;
 
@@ -31,14 +14,34 @@ const errorSelector = state => state.error;
 
 const columnsSelector = state => Object.values(state.columns).filter(column => column.visible);
 
-const sortParamsSelector = (state) => state.sortParams;
+const sortParamsSelector = state => state.sortParams;
 
-const groupParamsSelector = (state) => state.groupParams;
+const groupParamsSelector = state => state.groupParams;
 
-const searchSelector = (state) => state.searchString;
+const searchSelector = state => state.searchString;
+
+const dogsGroupReselector = createSelector(
+  dogsListSelector,
+  sortParamsSelector,
+  groupParamsSelector,
+  searchSelector,
+  (dogsList, sortParams, groupParams, searchString) => {
+    let result = dogsList;
+
+    if (searchString) {
+      result = filterDogsList(result, searchString);
+    };
+
+    result = sortDogsList(result, sortParams);
+
+    result = groupDogsList(result, groupParams);
+
+    return result;
+  }
+);
 
 const mapStateToProps = ({ dogsListState: state }) => ({
-  dogsGroups: dogsGroupsSelector(state),
+  dogsGroups: dogsGroupReselector(state),
   isLoading: isLoadingSelector(state),
   error: errorSelector(state),
   columns: columnsSelector(state),
